79108129e2e577ba17ee56ec8a3101ecf49f2b2b
David Alencar <davidalencarignacio@gmail.com>
2017-10-23 12:49:46 -0200

    change app declaretion keyword

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./config/routes')
 
-var app = express()
+const app = express()
 
 app.use(bodyParser.json())
 
